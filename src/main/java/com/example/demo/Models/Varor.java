2f8cac83b255cda45c61686e8ffd238c3a24a340
UrbanUsling <115050797+UrbanUsling@users.noreply.github.com>
2023-05-02 10:06:11 +0200

    Create Varor.java

---
+++ src/main/java/com/example/demo/Models/Varor.java
@@ -0,0 +1,29 @@
+package com.example.demo.RESTAPI;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Varor {
+
+    @Id
+    @GeneratedValue
+    protected long id;
+    protected String namn;
+    protected String pris;
+
+    public Varor(String namn, String pris){
+        this.namn = namn;
+        this.pris = pris;
+    }
+
+}
